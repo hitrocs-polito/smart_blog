988b01b0a500aca18730e86c392481f833a5f2fe
hitrocs-polito <su12057@studenti.polito.it>
2023-03-24 06:35:27 +0500

    Smart Blog

---
+++ accounts/forms.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from accounts.models import CustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm):
+        model = CustomUser
+        fields = ('username', 'first_name', 'last_name', 'email', 'age')
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm):
+        model = CustomUser
+        fields = ('username', 'first_name', 'last_name', 'email', 'age')
---
+++ accounts/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+
+# Create your views here.
+from .models import CustomUser
+
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
+
+
